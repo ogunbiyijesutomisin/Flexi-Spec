b1f8725176558cf61b5519f4227d6ba08b9a6188
ogunbiyijesutomisin <ogunbiyijesutomisin@gmail.com>
2026-08-17 17:35:17 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-const url=import.meta.env?.VITE_SUPABASE_URL || window.__SUPABASE_URL__ || '';
-const key=import.meta.env?.VITE_SUPABASE_PUBLISHABLE_KEY || window.__SUPABASE_PUBLISHABLE_KEY__ || '';
-export const supabase=createClient(url,key);
+
+const url = 'https://ayxrcahgjzppsudvoeuj.supabase.co';
+
+const key = 'sb_publishable_OVvrtk1VEmNh0AdKmxyLVA_E8APN9md';
+
+export const supabase = createClient(url, key);
